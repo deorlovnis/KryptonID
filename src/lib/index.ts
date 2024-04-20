2d37bad73ef0f1ddb3473987cfa943bd1823038c
v1rtl <hi@v1rtl.site>
2024-04-20 12:40:27 +0300

    make pk recovery work

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -23,20 +23,21 @@ export const getPublicKeyFromWebEID = async () => {
   const message = 'Signing in with KryptonID'
 
   const data = encoder.encode(message)
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+  const hashBuffer = await crypto.subtle.digest('SHA-384', data)
   const hashBinary = new Uint8Array(hashBuffer)
   const hashArray = Array.from(hashBinary)
 
-  const base64String = btoa(String.fromCharCode.apply(null, hashArray));
-  const hash = base64String.slice(0, 44)
+  const hash = btoa(String.fromCharCode.apply(null, hashArray));
 
   const { signature } = await webeid.sign(
     certificate,
     hash,
-    'SHA-256' as SignatureHashFunction,
+    'SHA-384' as SignatureHashFunction,
   )
 
-  const sig = p384.Signature.fromCompact(base64ToHex(signature))
+  let sig = p384.Signature.fromCompact(base64ToHex(signature))
 
-  const pubKey = sig.recoverPublicKey('idk').toHex()
+  sig = sig.addRecoveryBit(1)
+
+  console.log(sig.recoverPublicKey(hashBinary).toHex())
 }
