329b7452178b502e3d504bf66ca958c811939f43
v1rtl <hi@v1rtl.site>
2024-04-21 10:58:07 +0300

    send transaction

--- src/pages/wallet/app.ts
+++ src/pages/wallet/app.ts
@@ -1,4 +1,4 @@
-import { parseEther, zeroAddress } from 'viem'
+import { Address, formatEther, parseEther, zeroAddress } from 'viem'
 import {
   getWalletAddressWithPubkey,
   initWalletClient,
@@ -6,14 +6,39 @@ import {
   sendETH,
 } from '../../lib/eth'
 
-async function main() {
+const form = document.getElementById('form') as HTMLFormElement
+
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const currentTarget = e.currentTarget as HTMLFormElement
+
+  if (currentTarget.reportValidity()) {
+    const fd = new FormData(e.currentTarget as HTMLFormElement)
+
+    const wc = await initWalletClient()
+  
+    const { pubKey } = await getWalletAddressWithPubkey(publicClient, wc)
+  
+    sendETH(wc, pubKey, parseEther(fd.get('amount') as string), fd.get('addr') as Address)
+  }
+})
+
+
+setInterval(async () => {
+
   const wc = await initWalletClient()
+  
+  const { wallet } = await getWalletAddressWithPubkey(publicClient, wc)
 
-  const { pubKey, wallet } = await getWalletAddressWithPubkey(publicClient, wc)
+  const balance = await publicClient.getBalance({
+    address: wallet,
+  })
 
-  console.log(pubKey, wallet)
+  console.log(wallet)
 
-  sendETH(wc, pubKey, parseEther('1'), zeroAddress)
-}
+  const div = document.getElementById('balance') as HTMLElement
 
-main()
+  div.innerText = formatEther(balance)
+}, 1000)
